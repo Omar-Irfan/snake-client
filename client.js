78e918ee1947651538f4282f526c279f90bddeab
Omar Irfan <omar.irfan18@gmail.com>
2021-04-07 23:04:58 +0000

    refactor

--- client.js
+++ client.js
@@ -2,8 +2,8 @@ const net = require('net');
 const { IP, PORT } = require('./constants');
 const connect = function() {
   const conn = net.createConnection({ 
-    host: IP;
-    port: PORT;
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
