cdf4a99fc356dfdb801903e3f0f459df04156144
Omar Irfan <omar.irfan18@gmail.com>
2021-04-07 19:46:33 +0000

    game complete

--- input.js
+++ input.js
@@ -1,18 +1,36 @@
-const handleUserInput = function(key) {
+let connection;
+
+
+
+const handleUserInput = function(key, connection) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  } 
+  if (key === 's') {
+    connection.write("Move: down");
+  }  
+  if (key === 'd') {
+    connection.write("Move: right");
+  } 
+  
 }
 
-const setupInput = function() {
-const stdin = process.stdin;
-stdin.setRawMode(true);
-stdin.setEncoding('utf8');
-stdin.resume();
-stdin.on('data', key => {
-  handleUserInput(key);
-})
-return stdin;
-};
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key, connection);
+  })
+  return stdin;
+  };
 
 module.exports = { setupInput };
