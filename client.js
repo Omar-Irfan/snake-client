c4cfbd92c2da9d2bf6df091ea981610d1d00daff
Omar Irfan <omar.irfan18@gmail.com>
2021-04-07 17:41:41 +0000

    messed around with move

--- client.js
+++ client.js
@@ -1,16 +1,19 @@
 const net = require('net');
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
+    host: '135.23.222.131',
+    port: 50542
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
+  conn.on('connect', (connect) => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: 0I');
+  });
   conn.on('data', (data) => {
     console.log(data);
   });
-
+  
   return conn;
 }
-
 module.exports = { connect }
